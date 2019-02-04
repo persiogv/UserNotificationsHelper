bd704132736c43c24f50c30e8010ce0b74d7aec8
Pérsio Vieira <persiogv@gmail.com>
2019-02-04 02:29:30 -0200

    Update UserNotificationsHelper.swift

--- UserNotificationsHelper.swift
+++ UserNotificationsHelper.swift
@@ -7,6 +7,78 @@
 
 import UserNotifications
 
+enum UserNotificationsHelperError: Error {
+    case unauthorized
+    case unauthorizedOptions(options: UNAuthorizationOptions)
+    case unhandledError(error: Error)
+    case requestError(error: Error)
+}
+
 struct UserNotificationsHelper {
     
+    // MARK: - Public statements
+    
+    static func scheduleLocalNotification(withIdentifier identifier: String,
+                                          options: UNAuthorizationOptions,
+                                          content: UNNotificationContent,
+                                          trigger: UNCalendarNotificationTrigger?,
+                                          completion: @escaping (() throws -> Void) -> Void) {
+        center.getNotificationSettings { (settings) in
+            guard settings.authorizationStatus == .authorized else { return completion { throw UserNotificationsHelperError.unauthorized } }
+            
+            authorizedOptions { (results) in
+                do {
+                    let authorizedOptions = try results()
+                    if authorizedOptions.contains(options) {
+                        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                        center.add(request) { (error) in
+                            guard let error = error else { return completion {} }
+                            return completion { throw UserNotificationsHelperError.requestError(error: error) }
+                        }
+                    }
+                    
+                    completion {
+                        let invalidOptions = options.subtracting(authorizedOptions)
+                        throw UserNotificationsHelperError.unauthorizedOptions(options: invalidOptions)
+                    }
+                } catch {
+                    completion { throw error }
+                }
+            }
+        }
+    }
+    
+    static func requestAuthorization(for options: UNAuthorizationOptions, completion: @escaping (() throws -> Bool) -> Void) {
+        center.requestAuthorization(options: options) { (success, error) in
+            guard let error = error else { return completion { return success } }
+            completion { throw UserNotificationsHelperError.requestError(error: error) }
+        }
+    }
+    
+    static func authorizedOptions(completion: @escaping (() throws -> UNAuthorizationOptions) -> Void) {
+        center.getNotificationSettings { (settings) in
+            var options: UNAuthorizationOptions = []
+            if settings.badgeSetting == .enabled {
+                options.insert(.badge)
+            }
+            
+            if settings.soundSetting == .enabled {
+                options.insert(.sound)
+            }
+            
+            if settings.alertSetting == .enabled {
+                options.insert(.alert)
+            }
+            
+            if settings.carPlaySetting == .enabled {
+                options.insert(.carPlay)
+            }
+            
+            completion { return options }
+        }
+    }
+    
+    // MARK: - Private statements
+    
+    private static let center = UNUserNotificationCenter.current()
 }
